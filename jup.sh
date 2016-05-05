c10a4f2be7760ffbf5d2c6abf9b43c1a2e5006df
U-REDMOND\sadan <snarayan@SADAN1.redmond.corp.microsoft.com>
2016-05-04 23:46:32 -0700

    Commiting by Sada

--- jup.sh
+++ jup.sh
@@ -1,2 +1,3 @@
 #nohup /anaconda/bin/jupyter-notebook --no-browser --ip=* --notebook-dir /opt/SCHAS/git/Notebooks/
-nohup /anaconda/bin/jupyter-notebook --no-browser --ip=* --notebook-dir .
+#nohup /anaconda/bin/jupyter-notebook --no-browser --ip=* --notebook-dir .
+/anaconda/bin/jupyter-notebook --no-browser --ip=* --notebook-dir .
