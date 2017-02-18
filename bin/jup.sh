b739e37f44913795d28318cd923e11ecd9cf3976
sada-narayanappa <sada.narayanappa@gmail.com>
2017-02-18 08:51:37 -0700

    Commiting by Sada

--- bin/jup.sh
+++ bin/jup.sh
@@ -1,3 +1,3 @@
 #nohup /anaconda/bin/jupyter-notebook --no-browser --ip=* --notebook-dir /opt/SCHAS/git/Notebooks/
 #nohup /anaconda/bin/jupyter-notebook --no-browser --ip=* --notebook-dir .
-/anaconda/bin/jupyter-notebook  --ip=* --notebook-dir /opt/SCHAS/git/nn/Notebooks --port=8899
+/anaconda/bin/jupyter-notebook  --ip=* --notebook-dir /opt/SCHAS/git/Notebooks --port=8888
