647182dc8113bdb1f87a19a023a40dd9cad94d20
sada-narayanappa <sada.narayanappa@gmail.com>
2017-08-30 08:04:39 -0600

    Commiting by Sada

---
+++ bin/mlpull.sh
@@ -0,0 +1,3 @@
+ssh -i "schas.pem" centos@35.160.219.141 "cd /opt/SCHAS/git/Notebooks;git reset --hard origin/master;git pull"
+ssh -i "schas.pem" centos@35.160.219.141 "cd /opt/SCHAS/git/Notebooks/PyUtils;git reset --hard origin/master;git pull"
+
