45b3541ab198e9404e75e67c49834fd8d892cff2
NARAYANAPPA, Sadananda (US) <sadananda.narayanappa@lmco.com>
2019-03-29 07:47:36 -0600

    Commiting by Sada

--- Notes/ARX/c/utils/score/main.c
+++
@@ -1,5 +0,0 @@
-extern int main_score(int argc, char **argv);
-//-------------------------------------------------------------------------------
-int main(int argc, char **argv){
-    return main_score(argc, argv);
-}
