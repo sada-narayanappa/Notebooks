6b40e090139d135475c769e67f44c9d1c1ca3b75
NARAYANAPPA, Sadananda (US) <sadananda.narayanappa@lmco.com>
2019-09-01 07:27:02 -0600

    Commiting by Sada

--- Notes/ARX/c/utils/main.c
+++
@@ -1,5 +0,0 @@
-extern int main_invx(int argc, char **argv);
-//-------------------------------------------------------------------------------
-int main(int argc, char **argv){
-    main_invx(argc, argv);
-}
--- Notes/ARX/c/utils/model.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <stdarg.h>
-
-#include "linreg.h"
-#include "Common.h"
-
